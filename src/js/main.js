78d8bf86d8e39150f921d37fd6524b767206388f
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2025-03-26 21:32:35 -0300

    initial js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+const searchInput = document.getElementById("searchInput");
+const weatherCards = document.getElementById("weatherCards");
+
+const weatherConditions = {
+  clear: "sunny",
+  clouds: "cloudy",
+  rain: "rainy",
+  drizzle: "rainy",
+  thunderstorm: "rainy",
+  snow: "snowy",
+  mist: "foggy",
+  smoke: "foggy",
+  haze: "foggy",
+  fog: "foggy",
+};
 
