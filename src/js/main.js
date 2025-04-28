e01439bb4d93cd72f572fffa909ae2ea605e7b4b
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2025-04-28 17:38:51 -0300

    card css changes

--- src/js/main.js
+++ src/js/main.js
@@ -17,15 +17,34 @@ const weatherConditions = {
   fog: "foggy",
 };
 
+// função para buscar dados do clima
 async function fetchWeatherData(cityName) {
   try {
     const response = await fetch(
       `${BASE_URL}?q=${cityName}&appid=${API_KEY}&units=metric&lang=pt_br`
     );
-    if (!response.ok) throw new Error('Cidade não encontrada')
-      return await response.json();
+    if (!response.ok) throw new Error("Cidade não encontrada");
+    return await response.json();
   } catch (error) {
-    console.error('Erro ao buscar dados:', error);
+    console.error("Erro ao buscar dados:", error);
     return null;
   }
+}
+
+// função para criar cartão de clima
+function createWeatherCard(weatherData) {
+  const weatherId = weatherData.weather[0].main.toLowerCase();
+  const condition = weatherConditions[weatherId] || "unknown";
+
+  const card = document.createElement("div");
+  card.className = "weather-card";
+  card.dataset.temp = Math.round(weatherData.main.temp);
+  card.dataset.condition = condition;
+  card.dataset.wind = Math.round(weatherData.wind.speed);
+
+  card.innerHTML = `
+  <h2 class="city-name">${weatherData.name}, ${weatherData.sys.country}</h2>
+    <div class="weather-info">
+    
+  `
 }
